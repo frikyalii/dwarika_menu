07e334607bc4e8c278c5d098275948883cc303c4
Asad Ali <97747175+frikyalii@users.noreply.github.com>
2024-06-05 22:31:04 +0530

    Update jquery issue

--- src/KathaPage.js
+++ src/KathaPage.js
@@ -33,28 +33,8 @@ const KathaPage = () => {
           { name: 'Paper Plain Dosa', description: 'Beef patty with cheese', price: '₹8' },
           { name: 'Paper Masala Dosa', description: 'Vegetarian patty with veggies', price: '₹7' },
           { name: 'Dwarika Special Dosa', description: 'Vegetarian patty with veggies', price: '₹7' },
-  
         ]
       },
-      { 
-      //   category: 'Pizzas',
-      //   items: [
-      //     { name: 'Margherita Pizza', description: 'Tomato and cheese', price: '$10' },
-      //     { name: 'Pepperoni Pizza', description: 'Pepperoni and cheese', price: '$12' },
-      //   ]
-      // },
-      // { 
-      //   category: 'Uttappa',
-      //   items: [
-      //     { name: 'Cheeseburger', description: 'Beef patty with cheese', price: '$8' },
-      //     { name: 'Veggie Burger', description: 'Vegetarian patty with veggies', price: '$7' },
-      //     { name: 'Cheeseburger', description: 'Beef patty with cheese', price: '$8' },
-      //     { name: 'Veggie Burger', description: 'Vegetarian patty with veggies', price: '$7' },
-      //     { name: 'Cheeseburger', description: 'Beef patty with cheese', price: '$8' },
-      //     { name: 'Veggie Burger', description: 'Vegetarian patty with veggies', price: '$7' },
-      //   ]
-      },
-      // Add more categories as needed
     ],
     drinks: [
       {
@@ -74,69 +54,47 @@ const KathaPage = () => {
           { name: 'Plain Butter Milk', description: 'Freshly squeezed orange juice', price: '₹30' },
           { name: 'Masala Butter Milk', description: 'Freshly made lemonade', price: '₹40' },
           { name: 'Sweet Lassi', description: 'Freshly made lemonade', price: '₹' },
-
         ]
       },
-      // Add more categories as needed
     ],
-    // bakes: [
-    //   { name: 'Croissant', description: 'Buttery and flaky', price: '$3' },
-    //   { name: 'Muffin', description: 'Blueberry muffin', price: '$2.5' },
-    //   { name: 'Cupcake', description: 'Chocolate with vanilla icing', price: '$2' },
-    // ],
     events: [],
   };
 
   const renderMenu = () => {
-    const menu = menuData[selectedMenu] || [];
-    if (selectedMenu === 'bakes') {
-      return (
-        <div>
-          <h2>{selectedMenu.charAt(0).toUpperCase() + selectedMenu.slice(1)} Menu</h2>
-          <ul className="menu-list">
-            {menu.map((item, idx) => (
-              <li key={idx} className="menu-item">
-                <div className="item-info">
-                  <div className="item-name">{item.name}</div>
-                  <div className="item-description">{item.description}</div>
-                </div>
-                <div className="item-price">{item.price}</div>
-              </li>
-            ))}
-          </ul>
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <h2>{selectedMenu.charAt(0).toUpperCase() + selectedMenu.slice(1)} Menu</h2>
-          {menu.map((category, index) => (
-            <div key={index}>
-              <h3 className="text-danger food-menu">
-                {category.category}
-              </h3>
-              <ul className="menu-list">
-              {category.items && category.items.map((item, idx) => (
-                  <li key={idx} className="menu-item">
-                    <div className="item-info">
-                      <div className="item-name">{item.name}</div>
-                      {/* <div className="item-description">{item.description}</div> */}
-                    </div>
-                    <div className="item-price">{item.price}</div>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>
-      );
+    const menu = menuData[selectedMenu];
+  
+    if (!menu) {
+      return null; // or some default content
     }
+
+    return (
+      <div>
+        <h2>{selectedMenu.charAt(0).toUpperCase() + selectedMenu.slice(1)} Menu</h2>
+        {menu.map((category, index) => (
+          <div key={index}>
+            <h3 className="text-danger food-menu">
+              {category.category}
+            </h3>
+            <ul className="menu-list">
+              {category.items.map((item, idx) => (
+                <li key={idx} className="menu-item">
+                  <div className="item-info">
+                    <div className="item-name">{item.name}</div>
+                    {/* <div className="item-description">{item.description}</div> */}
+                  </div>
+                  <div className="item-price">{item.price}</div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
+    );
   };
 
   return (
     <div>
       <Container className="text-center my-5 text-success">
-        {/* <h1 className='py-5'>Welcome to Dwarika</h1> */}
         <Row className="justify-content-center mt-4 py-5">
           <Col xs={12} sm={6} md={3} className="mb-4">
             <Card 
@@ -163,30 +121,6 @@ const KathaPage = () => {
                 </Card.Text>
               </Card.Body>
             </Card>
-          </Col>
-          {/* <Col xs={12} sm={6} md={3} className="mb-4">
-            <Card 
-              className={`shadow-sm menu-card ${selectedMenu === 'bakes' ? 'active' : ''}`} 
-              onClick={() => handleMenuClick('bakes')}
-            >
-              <Card.Body>
-                <Card.Text>
-                  <span role="img" aria-label="croissant">🥐</span><br/>
-                  Bakes Menu
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </Col> */}
-          <Col xs={12} sm={6} md={3} className="mb-4">
-            {/* Uncomment and add events when available */}
-            {/* <Card className={`shadow-sm menu-card ${selectedMenu === 'events' ? 'active' : ''}`} onClick={() => handleMenuClick('events')}>
-              <Card.Body>
-                <Card.Text>
-                  <span role="img" aria-label="party">🥳</span><br/>
-                  Events @Katha
-                </Card.Text>
-              </Card.Body>
-            </Card> */}
           </Col>
         </Row>
         <div>
